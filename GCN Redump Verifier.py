e2b3ad9a00d741b5643c818e4d1512c15c8daaf6
normalgamer <36603719+normalgamer@users.noreply.github.com>
2020-09-01 11:22:36 +0200

    Add files via upload

---
+++ GCN Redump Verifier.py
@@ -0,0 +1,62 @@
+import os
+import hashlib
+
+datfile = "Nintendo - GameCube - Datfile (1907) (2020-08-28 21-47-09).dat"
+
+read_size = 1024
+hash = hashlib.md5()
+
+print(""
+    +"==================== Redump verifier - version 1.0 ===================="
+    +"\n"
+    +"--------------------    Github.com/normalgamer     --------------------\n"
+    +"\n"
+    +"Drag and drop your Gamecube ISO into this window"
+    +"\n"
+)
+
+i = input("> ")
+inputFile = i.replace("\"", "")
+
+with open(inputFile, "rb") as f:
+    data = f.read(read_size)
+    while data:
+        hash.update(data)
+        data = f.read(read_size)
+hash = hash.hexdigest()
+
+line_number=0
+name_line=0
+
+with open(datfile) as f:
+    if hash in f.read():
+        print("\n"
+        +"ISO's MD5 hash: " + hash +"\n"
+        +"\n"
+        +"Game verified, ISO's MD5 matches Redump hash"
+        )
+    else:
+        print("\n"
+        +"ISO's MD5 doesn't match any Gamecube game MD5"
+        )
+    f.close()
+
+with open(datfile) as f:
+    for line in f:
+        line_number+=1
+        if hash in line:
+            name_line=line_number-1
+            file = open(datfile)
+            all_lines = file.readlines()
+            gameName=all_lines[name_line-1].replace("<description>","")
+            gameName=gameName.replace("</description>","")
+            gameName=gameName.replace("\t","")
+    
+            #print(all_lines[name_line-1])
+            print("Redump game name: "+gameName)
+            file.close()
+    
+    f.close()
+    
+        
+input("\n")
