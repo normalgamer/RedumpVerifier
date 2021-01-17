b2ef6cb6eba9279b7a75ed4e6d9a239b58e28e34
normalgamer <36603719+normalgamer@users.noreply.github.com>
2021-01-17 20:47:40 +0100

    Added use of argument as file\/directory to verify

--- RedumpVerifier.py
+++ RedumpVerifier.py
@@ -1,42 +1,32 @@
 import os
 import hashlib
+import sys
+
 
 dats = os.listdir("./dat")
 read_size = 1024
 hash = hashlib.md5()
 sha_256 = hashlib.sha256()
 gameVerified = False
-line_number=0
-
+line_number = 0
 
 
 def getAllFiles(dirName):
     files = os.listdir(dirName)
-    
     allFiles = list()
-    
+
     for entry in files:
         fullpath = os.path.join(dirName, entry)
         if os.path.isdir(fullpath):
             allFiles = allFiles + getAllFiles(fullpath)
         else:
             allFiles.append(fullpath)
-    
+
     return allFiles
 
-
-print(""
-    +"==================    Redump verifier - version 1.6   ====================\n"
-    +"------------------       Github.com/normalgamer       --------------------\n"
-    +"\n"
-    +"Drag 'n Drop your ISO or folder\n"
-    +"\n"
-    )
-userInput = input("> ")
-
-
-if os.path.isfile(userInput.replace("\"","")):  # if input is a file
-    iso = userInput.replace("\"","")
+def verify(file):
+    gameVerified = False
+    iso = file.replace("\"","")
     hash = hashlib.md5()
     print("\nCalculating hash...")
     with open(iso, "rb") as f:
@@ -45,8 +35,6 @@ if os.path.isfile(userInput.replace("\"","")):  # if input is a file
             hash.update(data)
             data = f.read(read_size)
     hash = hash.hexdigest()
-
-
 
     for dat in dats:
         line_number = 0
@@ -67,7 +55,7 @@ if os.path.isfile(userInput.replace("\"","")):  # if input is a file
                     gameName = data[name_line].replace("<description>", "").replace("</description>", "").replace("\t", "")
                     print("\nRedump game name: " + gameName)
                     gameVerified = True
-        
+
 
         if gameVerified:
             break
@@ -79,29 +67,26 @@ if os.path.isfile(userInput.replace("\"","")):  # if input is a file
              +"ISO's MD5 doesn't match any Redump hash"
              )
 
+def verify_multiple(files):
 
-    input()
-    
+    romList = list()
+    romListRedumpName = list()
+    romListHash = list()
 
-
-
-elif os.path.isdir(userInput.replace("\"","")):  # if input is a folder
-    
-    inputFolder = userInput.replace("\"","")
-    inputFiles = getAllFiles(inputFolder)
-    
-    for iso in inputFiles:
+    for file in files:
+        gameVerified = False
+        iso = file.replace("\"","")
         print("\nISO: " + iso)
         hash = hashlib.md5()
-        print("\nCalculating hash...")
+        print("Calculating hash...")
         with open(iso, "rb") as f:
             data = f.read(read_size)
             while data:
                 hash.update(data)
                 data = f.read(read_size)
-            hash = hash.hexdigest()
+        hash = hash.hexdigest()
 
-        gameVerified = False
+        romList.append(file)
 
         for dat in dats:
             line_number = 0
@@ -111,10 +96,10 @@ elif os.path.isdir(userInput.replace("\"","")):  # if input is a folder
                 for line in data:
                     line_number += 1
                     if hash in line:
-                        print("ISO's MD5 hash: " + hash
+                        print("\n"
+                             +"ISO's MD5 hash: " + hash
                              +"\n"
                              +"Game Verified, ISO's MD5 matches Redump hash"
-                             +"\n"
                              )
                         name_line = line_number
                         while "<description>" not in data[name_line]:
@@ -125,8 +110,8 @@ elif os.path.isdir(userInput.replace("\"","")):  # if input is a folder
                              +"----------------------------------------")
                         gameVerified = True
 
-
             if gameVerified:
+                romListRedumpName.append(gameName)
                 break
 
         if not gameVerified:
@@ -137,11 +122,51 @@ elif os.path.isdir(userInput.replace("\"","")):  # if input is a folder
                  +"\n"
                  +"----------------------------------------"
                  )
+            romListRedumpName.append("Not verified")
+
+        romListHash.append(hash)
+
+    print("\n\nSummary:\n")
+    for rom in romList:
+        print(os.path.basename(rom) + " - " + romListRedumpName[romList.index(rom)])
+        print(romListHash[romList.index(rom)])
+        print("------------------------------\n")
 
 
-    input()
-    
-    
+# --------------------------------------------------------------------------- #
+
+if len(sys.argv > 1):
+    if os.path.isfile(sys.argv[1].replace("\"", "")):  # if input is a file
+        verify(sys.argv[1])
+        input()
+
+    elif os.path.isdir(sys.argv[1].replace("\"", "")):  # if input is a folder
+        inputFolder = sys.argv[1].replace("\"", "")
+        inputFiles = getAllFiles(inputFolder)
+        verify_multiple(inputFiles)
+
+    else:
+        print("Something went wrong...")
+
 
 else:
-    print("Something went wrong...")
+    print(""
+        +"==================    Redump verifier - version 1.7   ====================\n"
+        +"------------------       Github.com/normalgamer       --------------------\n"
+        +"\n"
+        +"Drag 'n Drop your ISO or folder\n"
+        +"\n"
+        )
+    userInput = input("> ")
+
+    if os.path.isfile(userInput.replace("\"","")):  # if input is a file
+        verify(userInput)
+        input()
+
+    elif os.path.isdir(userInput.replace("\"","")):  # if input is a folder
+        inputFolder = userInput.replace("\"","")
+        inputFiles = getAllFiles(inputFolder)
+        verify_multiple(inputFiles)
+
+    else:
+        print("Something went wrong...")
