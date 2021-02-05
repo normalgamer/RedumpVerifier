ed525a84374abd96b139301f54aec345ecd3dd75
normalgamer <36603719+normalgamer@users.noreply.github.com>
2021-02-05 22:28:31 +0100

    v1.8

--- RedumpVerifier.py
+++ RedumpVerifier.py
@@ -6,9 +6,13 @@ import sys
 dats = os.listdir("./dat")
 read_size = 1024
 hash = hashlib.md5()
-sha_256 = hashlib.sha256()
 gameVerified = False
 line_number = 0
+
+romList = list()
+romListRedumpName = list()
+romListHash = list()
+
 
 
 def getAllFiles(dirName):
@@ -24,58 +28,12 @@ def getAllFiles(dirName):
 
     return allFiles
 
-def verify(file):
-    gameVerified = False
-    iso = file.replace("\"","")
-    hash = hashlib.md5()
-    print("\nCalculating hash...")
-    with open(iso, "rb") as f:
-        data = f.read(read_size)
-        while data:
-            hash.update(data)
-            data = f.read(read_size)
-    hash = hash.hexdigest()
 
-    for dat in dats:
-        line_number = 0
-        data = ""
-        with open("dat/" + dat) as f:
-            data = f.readlines()
-            for line in data:
-                line_number += 1
-                if hash in line:
-                    print("\n"
-                         +"ISO's MD5 hash: " + hash
-                         +"\n"
-                         +"Game Verified, ISO's MD5 matches Redump hash"
-                         )
-                    name_line = line_number
-                    while "<description>" not in data[name_line]:
-                        name_line -= 1
-                    gameName = data[name_line].replace("<description>", "").replace("</description>", "").replace("\t", "")
-                    print("\nRedump game name: " + gameName)
-                    gameVerified = True
-
-
-        if gameVerified:
-            break
-
-    if not gameVerified:
-        print("\n"
-             +"ISO's MD5: " + hash
-             +"\n"
-             +"ISO's MD5 doesn't match any Redump hash"
-             )
-
-def verify_multiple(files):
-
-    romList = list()
-    romListRedumpName = list()
-    romListHash = list()
-
+def verify(files):
+    global romList, romListRedumpName, romListHash
     for file in files:
         gameVerified = False
-        iso = file.replace("\"","")
+        iso = file.replace("\"", "")
         print("\nISO: " + iso)
         hash = hashlib.md5()
         print("Calculating hash...")
@@ -126,47 +84,65 @@ def verify_multiple(files):
 
         romListHash.append(hash)
 
-    print("\n\nSummary:\n")
-    for rom in romList:
-        print(os.path.basename(rom) + " - " + romListRedumpName[romList.index(rom)])
-        print(romListHash[romList.index(rom)])
-        print("------------------------------\n")
-
 
 # --------------------------------------------------------------------------- #
 
 if len(sys.argv) > 1:
-    if os.path.isfile(sys.argv[1].replace("\"", "")):  # if input is a file
-        verify(sys.argv[1])
-        input()
+    for i in sys.argv[1:]:
+        if os.path.isfile(i.replace("\"", "")):  # if input is a file
+            input_file = list()
+            input_file.append(i)
+            verify(input_file)
 
-    elif os.path.isdir(sys.argv[1].replace("\"", "")):  # if input is a folder
-        inputFolder = sys.argv[1].replace("\"", "")
-        inputFiles = getAllFiles(inputFolder)
-        verify_multiple(inputFiles)
+        elif os.path.isdir(i.replace("\"", "")):  # if input is a folder
+            inputFolder = i.replace("\"", "")
+            inputFiles = getAllFiles(inputFolder)
+            verify(inputFiles)
 
-    else:
-        print("Something went wrong...")
+        else:
+            print("Something went wrong...")
+
+    #if len(sys.argv) < 2:
+    print("\n\nSummary:\n")
+    for rom in romList:
+        print(os.path.basename(rom) + " - " + romListRedumpName[romList.index(rom)])
+        print(romListHash[romList.index(rom)])
+
+    input()
 
 
 else:
     print(""
-        +"==================    Redump verifier - version 1.7   ====================\n"
+        +"==================    Redump verifier - version 1.8   ====================\n"
         +"------------------       Github.com/normalgamer       --------------------\n"
         +"\n"
         +"Drag 'n Drop your ISO or folder\n"
+        +"If you want to verify multiple items, separate them by an asterisk \( \* \)\n"
         +"\n"
         )
+
     userInput = input("> ")
+    userInput = userInput.split("*")
 
-    if os.path.isfile(userInput.replace("\"","")):  # if input is a file
-        verify(userInput)
-        input()
+    for i in userInput:
+        print(i)
+        if os.path.isfile(i.replace("\"", "")):  # if input is a file
+            input_file = list()
+            input_file.append(i)
+            verify(input_file)
 
-    elif os.path.isdir(userInput.replace("\"","")):  # if input is a folder
-        inputFolder = userInput.replace("\"","")
-        inputFiles = getAllFiles(inputFolder)
-        verify_multiple(inputFiles)
+        elif os.path.isdir(i.replace("\"", "")):  # if input is a folder
+            inputFolder = i.replace("\"", "")
+            inputFiles = getAllFiles(inputFolder)
+            verify(inputFiles)
 
-    else:
-        print("Something went wrong...")
+        else:
+            print("Something went wrong...")
+
+    #if len(sys.argv) < 2:
+    print("\n\nSummary:\n")
+    for rom in romList:
+        print(os.path.basename(rom) + " - "+ romListRedumpName[romList.index(rom)])
+        print(romListHash[romList.index(rom)])
+
+    input()
