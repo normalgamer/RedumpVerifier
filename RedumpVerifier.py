8b799bbc912d9d0f5b3c60ec50c53720039b4a5a
normalgamer <36603719+normalgamer@users.noreply.github.com>
2021-07-06 19:04:36 +0200

    v1.9

--- RedumpVerifier.py
+++ RedumpVerifier.py
@@ -1,6 +1,7 @@
 import os
 import hashlib
 import sys
+import datetime
 
 
 dats = os.listdir("./dat")
@@ -12,7 +13,6 @@ line_number = 0
 romList = list()
 romListRedumpName = list()
 romListHash = list()
-
 
 
 def getAllFiles(dirName):
@@ -55,17 +55,20 @@ def verify(files):
                     line_number += 1
                     if hash in line:
                         print("\n"
-                             +"ISO's MD5 hash: " + hash
-                             +"\n"
-                             +"Game Verified, ISO's MD5 matches Redump hash"
-                             )
+                              + "ISO's MD5 hash: " + hash
+                              + "\n"
+                              + "Game Verified, ISO's MD5 matches Redump hash"
+                              )
                         name_line = line_number
                         while "<description>" not in data[name_line]:
                             name_line -= 1
-                        gameName = data[name_line].replace("<description>", "").replace("</description>", "").replace("\t", "")
+                        gameName = data[name_line]\
+                            .replace("<description>", "")\
+                            .replace("</description>", "")\
+                            .replace("\t", "")
                         print("Redump game name: " + gameName
-                             +"\n"
-                             +"----------------------------------------")
+                              + "\n"
+                              + "----------------------------------------")
                         gameVerified = True
 
             if gameVerified:
@@ -74,18 +77,34 @@ def verify(files):
 
         if not gameVerified:
             print("ISO's MD5: " + hash
-                 +"\n"
-                 +"ISO's MD5 doesn't match any Redump hash"
-                 +"\n"
-                 +"\n"
-                 +"----------------------------------------"
-                 )
+                  + "\n"
+                  + "ISO's MD5 doesn't match any Redump hash"
+                  + "\n"
+                  + "\n"
+                  + "----------------------------------------"
+                  )
             romListRedumpName.append("Not verified")
 
         romListHash.append(hash)
 
 
 # --------------------------------------------------------------------------- #
+# Check for updates #
+with open("dat/_last_update", "r") as f:
+    last_update_time = f.read()
+
+
+if (int(str(datetime.date.today()).split("-")[1]) >
+    int(last_update_time.split("-")[1])) \
+   | (int(str(datetime.date.today()).split("-")[0]) >
+      int(last_update_time.split("-")[0])):
+    print("DATs haven't been updated in at least one month")
+    i = input("Do you want to update them? (y/n) > ")
+    if i == "y":
+        exec(open("dat_updater.py").read())
+    else:
+        pass
+
 
 if len(sys.argv) > 1:
     for i in sys.argv[1:]:
@@ -102,10 +121,11 @@ if len(sys.argv) > 1:
         else:
             print("Something went wrong...")
 
-    #if len(sys.argv) < 2:
+    # if len(sys.argv) < 2:
     print("\n\nSummary:\n")
     for rom in romList:
-        print(os.path.basename(rom) + " - " + romListRedumpName[romList.index(rom)])
+        print(os.path.basename(rom) + " - " +
+              romListRedumpName[romList.index(rom)])
         print(romListHash[romList.index(rom)])
 
     input()
@@ -113,13 +133,14 @@ if len(sys.argv) > 1:
 
 else:
     print(""
-        +"==================   Redump verifier - version 1.8b   ====================\n"
-        +"------------------       Github.com/normalgamer       --------------------\n"
-        +"\n"
-        +"Drag 'n Drop your ISO or folder\n"
-        +"If you want to verify multiple items, separate them by an asterisk ( * )\n"
-        +"\n"
-        )
+          + "============   Redump verifier - version 1.8b   ==============\n"
+          + "------------       Github.com/normalgamer       --------------\n"
+          + "\n"
+          + "Drag 'n Drop your ISO or folder\n"
+          + "If you want to verify multiple items, separate them by an"
+          + "asterisk ( * )\n"
+          + "\n"
+          )
 
     userInput = input("> ")
     userInput = userInput.split("*")
@@ -139,10 +160,11 @@ else:
         else:
             print("Something went wrong...")
 
-    #if len(sys.argv) < 2:
+    # if len(sys.argv) < 2:
     print("\n\nSummary:\n")
     for rom in romList:
-        print(os.path.basename(rom) + " - "+ romListRedumpName[romList.index(rom)])
+        print(os.path.basename(rom) + " - " +
+              romListRedumpName[romList.index(rom)])
         print(romListHash[romList.index(rom)])
 
     input()
